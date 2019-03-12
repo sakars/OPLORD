69c75bda2b37bd4fe419925d9bd422102f259d41
Oskars <o.putaans@gmail.com>
2019-03-12 20:32:24 +0200

    New function added that lets you find a stop using it's id.

--- Stops.js
+++ Stops.js
@@ -3,4 +3,14 @@ function Stop(name, E, N,Id){
     this.degE=E;
     this.degN=N;
     this.Id=Id;
+    IDstop.push([this.Id, this]);
 }
+Stop.prototype.findById=function(Id){
+  for(var i=0;i<IDstop;i++){
+    if(IDstop[i][0]==Id){
+      return IDstop[i][1];
+    }
+  }
+  throw "this ain't it chief";
+}
+var IDstop=[];
