3ed8558003d3cbd129bc0e323e1334d779f3f8c4
Oskars <o.putaans@gmail.com>
2019-03-18 11:48:45 +0200

    something

--- recycler.js
+++ recycler.js
@@ -5,28 +5,19 @@ var reA=[];//returned array
 var done=false;
 var i=0;
 function daritajs(){
-  i++;
-  if(i==2){
-    var bigA=str.split("\t");
-    var IDA=IDs.split("\t");
-    var IDSaf=[];
-    reA=[];
-    var name;
-    var E;
-    var N;//name N E
-    var Id;
-    while(bigA.length !=0){
-      E=Number(bigA.pop());
-      N=Number(bigA.pop());
-      name=bigA.pop();
-      Id=Number(IDA.pop());
-      if(!IDSaf.includes(Id)){
-        reA.push(new Stop(name,E,N,Id));
-        IDSaf.push(Id);
+  var bigA=str.split("\n");
+  var IDSaf=[];
+  reA=[];
+  bigA.forEach(
+    function(a,i){
+      bigA[i]=a.split("\t");
+      if(!IDSaf.includes(bigA[i][3])){
+        IDSaf.push(bigA[i][3]);
+        reA.push(new Stop(bigA[i][0],bigA[i][2],bigA[i][1],bigA[i][3]));
       }
     }
-    done =true;
-  }
+  );
+  done=true;
 }
 function recycle(){
   var fileURL = "Stop_Data";
@@ -40,14 +31,4 @@ function recycle(){
     }
   }
   xhr.send();
-  var fileURL2 = "BusID";
-  var xhr2 = new XMLHttpRequest();
-  xhr2.open('GET', fileURL2);
-  xhr2.onreadystatechange = function() {
-    if (xhr2.readyState == 4 && xhr2.status == 200) {
-      IDs=xhr2.responseText;
-      daritajs();
-    }
-  }
-  xhr2.send();
 }
