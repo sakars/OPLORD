db38f26765f7971703733aee10a2f25b3c8f6326
Saecl <saeclmusic@gmail.com>
2019-03-21 09:03:24 +0200

    variable day length

--- daycycle.js
+++ daycycle.js
@@ -2,24 +2,25 @@ var localTime = 0;
 var day = 0;
 var showDay = false;
 var pod = "";
+var dayLength = 1200;
 
 function updateTime(){
   //1200 secs
-  if(localTime < 1200){
-    let partOfDay = 24*localTime/1200;
+  if(localTime < dayLength){
+    let partOfDay = 24*localTime/dayLength;
     let minutes = Math.floor((partOfDay - Math.floor(partOfDay))*60);
     minutes = String(minutes).length == 2 ? minutes : "0" + minutes;
     let hours = Math.floor(partOfDay)
     hours = String(hours).length == 2 ? hours : "0" + hours;
     timeDisplay.innerHTML = hours + ":" + minutes;
     dayDisplay.innerHTML = "  Day: " + day;
-    if(localTime > 900){
+    if(localTime > dayLength*3/4){
       pod = "Evening";
     }
-    else if(localTime > 600){
+    else if(localTime > dayLength/2){
       pod = "Day";
     }
-    else if(localTime > 300){
+    else if(localTime > dayLength/4){
       pod = "Morning";
     }
     else{
